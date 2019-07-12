f305ac4011b1f779605cb78093f7a3bd8f878c70
Abay <art_ab00@mail.ru>
2019-07-13 04:19:35 +0600

    aguajg

--- logcreds.py
+++ logcreds.py
@@ -2,9 +2,12 @@
 
 TOKEN = '754569723:AAEk4CYExqx711a_r98VOvgTfe4533vUB5w'
 
-#players = []
+# players = []
+
+players = []
 
 team_a = []
 team_b = []
 
-verified_users = ['Shaneliya', 'treoa', 'prinnydood']
+
+verified_users = ['Shaneliya', 'treoa', 'prinnydood', 'tomo_yo']
--- teamificatorbot.py
+++ teamificatorbot.py
@@ -1,33 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import telebot
-# import sys
+from random import shuffle
+from string import ascii_uppercase
+
 from logcreds import *
-from user import *
 
 bot = telebot.TeleBot(TOKEN)
-players = []
-
-# registered users = ['Shaneliya', 'treoa', 'prinnydood']
-
-teama = []
-teamb = []
 
 
-def divideteams():  # divides into two teams
-    i = 0
-    for v in players:
-        if not thereisinteams(v):
-            teama.append(v) if (i % 2 == 0) else teamb.append(v)
-        i += 1
-
-
-def thereis(usr):
-    return usr in players
-
-
-def thereisinteams(usr):
-    return (usr in teama) or (usr in teamb)
+def divideteams(players):
+    shuffle(players)
+    mid = len(players) // 2
+    return players[:mid], players[mid:]
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -42,16 +27,22 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['reg'])
 def add_player(message):
-    if thereis(message.from_user.username):
+    if message.from_user.username in players:
+        bot.send_message(message.chat.id, "Я фсио видэль. Тебя точно видэль")
+    else:
         players.append(message.from_user.username)
         bot.reply_to(message, "Теперь ты официальный КСер XD")
-    else:
-        bot.send_message(message.chat.id, "Я фсио видэль. Тебя точно видэль")
+
+
+@bot.message_handler(commands=['add'])  # adds dummy players
+def add_dummies(message):
+    for ch in ascii_uppercase:
+        players.append(ch * 8)
 
 
 @bot.message_handler(commands=['immaout'])
 def remove_player(message):
-    if thereis(message.from_user.username):
+    if message.from_user.username in players:
         bot.reply_to(message, "Ухади отсюда, мужик!")
         players.remove(message.from_user.username)
     else:
@@ -62,18 +53,20 @@ def remove_player(message):
 def printing(message):
     if message.from_user.username in verified_users:
         for player in players:
-            bot.send_message(message.chat.id, player)
-            bot.send_message(message.chat.id, "Больше нит КСеров")
+            bot.send_message(message.chat.id, "@" + player)
+        bot.send_message(message.chat.id, "Больше нит КСеров")
 
 
-@bot.message_handler(commands=['print_teams'])  # divides and prints into teams
+@bot.message_handler(commands=['print_teams'])
 def printteams(message):
-    divideteams()
-    bot.send_message(message.chat.id, "TEAM A:")
-    for ta in teama:
+    team_a, team_b = divideteams(players)
+
+    bot.send_message(message.chat.id, "TEAM A: " + str(len(team_a)) + " players")
+    for ta in team_a:
         bot.send_message(message.chat.id, ta)
-    bot.send_message(message.chat.id, "TEAM B:")
-    for tb in teamb:
+
+    bot.send_message(message.chat.id, "TEAM B: " + str(len(team_b)) + " players")
+    for tb in team_b:
         bot.send_message(message.chat.id, tb)
 
 
