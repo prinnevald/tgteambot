e7f759da8adcf638a91a23fb9c7d7a6533c5c7a1
prinnevald <prinnyprinnevald@gmail.com>
2019-07-16 16:29:59 +0600

    Okay, so for now we hace no syntax issues regarding using the SQLite in the Python.But, we still have threading issues...

--- teamificatorbot.py
+++ teamificatorbot.py
@@ -8,15 +8,15 @@ from string import ascii_uppercase
 from logcreds import *
 
 bot = telebot.TeleBot(TOKEN)
-conn = sqlite3.connect('players.sqlite', check_same_thread = False)
+conn = sqlite3.connect('players.sqlite')#, check_same_thread = False)
 cur = conn.cursor()
 
 #bot.threaded = False
 
-def divideteams(players):
-    shuffle(players)
-    mid = len(players) // 2
-    return players[:mid], players[mid:]
+# def divideteams(players):
+#     shuffle(players)
+#     mid = len(players) // 2
+#     return players[:mid], players[mid:]
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -24,18 +24,18 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['reg'])
 def add_player(message):
-    if cur.execute("SELECT EXISTS(SELECT * FROM data WHERE username = '{}')".format("@" + message.from_user.username)) == 1:
+    if cur.execute("SELECT EXISTS(SELECT * FROM data WHERE username = '?')", ("@" + message.from_user.username)) == 1:
         bot.send_message(message.chat.id, "Я фсио видэль. Тебя точно видэль")
     else:
-        cur.execute("INSERT INTO data (username) VALUES '{}'".format("@" + message.from_user.username))
+        cur.execute("INSERT INTO data (username) VALUES '?'",("@" + message.from_user.username))
         conn.commit()
         bot.reply_to(message, "Теперь ты официальный КСер XD")
     cur.cancel()
         
 @bot.message_handler(commands=['immaout'])
 def remove_player(message):
-    if cur.execute("SELECT EXISTS(SELECT * FROM data WHERE username = '{}')".format("@" + message.from_user.username)) == 1:
-        cur.execute("DELETE FROM data WHERE username = '{}')".format("@" + message.from_user.username))  #remove from table
+    if cur.execute("SELECT EXISTS(SELECT * FROM data WHERE username = '?')",("@" + message.from_user.username)) == 1:
+        cur.execute("DELETE FROM data WHERE username = '?')",("@" + message.from_user.username))  #remove from table
         conn.commit()
         bot.reply_to(message, "Ухади отсюда, мужик!")
     else:
@@ -48,32 +48,32 @@ def printing(message):
         print(list(cur.execute("SELECT username FROM data")[0][0]))
         bot.send_message(message.chat.id, "Больше нит КСеров")
     else:
-        bot.send_message(message.chat.id, "Yo a not mah masta")
+        bot.send_message(message.chat.id, "Yo r not mah masta")
     cur.cancel()
 
-@bot.message_handler(commands=['print_teams'])
-def printteams(message):
-    team_a, team_b = divideteams(players)
+# @bot.message_handler(commands=['print_teams'])
+# def printteams(message):
+#     team_a, team_b = divideteams(players)
 
-    bot.send_message(message.chat.id, "TEAM A: " + str(len(team_a)) + " players")
-    for ta in team_a:
-        bot.send_message(message.chat.id, ta)
+#     bot.send_message(message.chat.id, "TEAM A: " + str(len(team_a)) + " players")
+#     for ta in team_a:
+#         bot.send_message(message.chat.id, ta)
 
-    bot.send_message(message.chat.id, "TEAM B: " + str(len(team_b)) + " players")
-    for tb in team_b:
-        bot.send_message(message.chat.id, tb)
+#     bot.send_message(message.chat.id, "TEAM B: " + str(len(team_b)) + " players")
+#     for tb in team_b:
+#         bot.send_message(message.chat.id, tb)
 
 @bot.message_handler(commands=['add_dummies'])  # adds dummy players
 def add_dummies(message):
     for ch in ascii_uppercase:
-        cur.execute("INSERT INTO data (username) VALUES '{}'".format("@" + ch * 8))
+        cur.execute("INSERT INTO data (username) VALUES '?'",("@" + ch * 8))
         conn.commit()
         cur.cancel()
 
 @bot.message_handler(commands=['delete_dummies'])  # delete dummy players
 def delete_dummies(message):
     for ch in ascii_uppercase:
-        cur.execute("DELETE FROM data WHERE username = '{}'".format("@" + ch * 8))
+        cur.execute("DELETE FROM data WHERE username = '?'",("@" + ch * 8))
         conn.commit()
         cur.cancel()
 
