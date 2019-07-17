47c3ee0c9217b5f552c14a6dbb39b06259a79c92
prinnevald <prinnyprinnevald@gmail.com>
2019-07-17 22:18:30 +0600

    refactoring code #2

--- teamificatorbot.py
+++ teamificatorbot.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import telebot
-from random import shuffle
 import sqlite3 as sq
+from random import shuffle
 from string import ascii_uppercase
-
 from logcreds import *
 
 bot = telebot.TeleBot(TOKEN)
 
-#############################################################################
-
-def dbup():
+def dbup():		#connect db
     conn = sq.connect('database.db')
     cur = conn.cursor()
     return cur, conn
 
-def dbdown(conn):    
+
+def dbdown(conn):	#disconnect db
     conn.commit()
     conn.close()
 
-def verified(msg):
+
+def verified(msg):	#if admin then true
 	return msg in verified_users
 
-def divideteams():
+
+def divideteams():	#extracts players from db and forms teams
     cur, conn = dbup()
     cur.execute("SELECT * FROM users")
     players = cur.fetchall()
@@ -34,43 +34,41 @@ def divideteams():
 
 #############################################################################
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help'])	#introductory msg
 def send_welcome(message):
     bot.send_message(message.chat.id, intromsg)
 
-@bot.message_handler(commands=['allo'])
+
+@bot.message_handler(commands=['allo'])	#just a check
 def allo(message):
     bot.send_message(message.chat.id, "Moshi, Moshi?")
 
-#############################################################################
 
-@bot.message_handler(commands=['reg'])
+@bot.message_handler(commands=['reg'])	#adds player into database
 def add_player(message):
     cur, conn = dbup()
-    cur.execute("SELECT * FROM users WHERE username=?", (message.from_user.username,))
+    cur.execute("SELECT * FROM users WHERE username=?", [message.from_user.username])
     if len(cur.fetchall()) == 0:
-        cur.execute("INSERT INTO users VALUES (?)", (message.from_user.username,))
+        cur.execute("INSERT INTO users VALUES (?)", [message.from_user.username])
         bot.reply_to(message, "Теперь ты официальный КСер XD")
     else:
         bot.send_message(message.chat.id, "Я фсио видэль. Тебя точно видэль")   
     dbdown(conn)
 
-#############################################################################
 
-@bot.message_handler(commands=['immaout'])
+@bot.message_handler(commands=['immaout'])	#deletes the record of the player from db
 def remove_player(message):
     cur, conn = dbup()
-    cur.execute("SELECT * FROM users WHERE username=?", (message.from_user.username,))
+    cur.execute("SELECT * FROM users WHERE username=?", [message.from_user.username])
     if len(cur.fetchall()) > 0:
-        cur.execute("DELETE FROM users WHERE username=?", (message.from_user.username,))
+        cur.execute("DELETE FROM users WHERE username=?", [message.from_user.username])
         bot.reply_to(message, "Ухади отсюда, мужик!")
     else:
         bot.reply_to(message, "Котом Шрёдингера запахло")
     dbdown(conn)
 
-#############################################################################
 
-@bot.message_handler(commands=['print_all'])
+@bot.message_handler(commands=['print_all'])	#prints every player
 def printing(message):
     if verified(message.from_user.username):
         cur, conn = dbup()   
@@ -85,9 +83,8 @@ def printing(message):
     else:
         bot.reply_to(message, "U r not mah masta")
 
-#############################################################################
 
-@bot.message_handler(commands=['print_teams'])
+@bot.message_handler(commands=['print_teams'])	#divides and prints out teams
 def printteams(message):
     if verified(message.from_user.username):
         team_a, team_b = divideteams()
@@ -102,41 +99,38 @@ def printteams(message):
     else:
         bot.reply_to(message, "U r not mah masta")
 
-#############################################################################
 
-@bot.message_handler(commands=['add_dummies'])  # adds dummy players
+@bot.message_handler(commands=['add_dummies'])	#adds dummy players for testing
 def add_dummies(message):
     if verified(message.from_user.username):
         cur, conn = dbup()
         for ch in ascii_uppercase:
-            cur.execute("SELECT * FROM users WHERE username=?", (ch * 8,))
+            cur.execute("SELECT * FROM users WHERE username=?", [ch * 8])
             if len(cur.fetchall()) == 0:
-                cur.execute("INSERT INTO users VALUES (?)", (ch * 8,))
+                cur.execute("INSERT INTO users VALUES (?)", [ch * 8])
 
         bot.reply_to(message, "Done")    
         dbdown(conn)
     else:
         bot.reply_to(message, "U r not mah masta")
 
-#############################################################################    
 
-@bot.message_handler(commands=['delete_dummies'])  # adds dummy players
+@bot.message_handler(commands=['delete_dummies'])  #deletes dummy players for testing
 def delete_dummies(message):
     if verified(message.from_user.username):
         cur, conn = dbup()
         for ch in ascii_uppercase:
-            cur.execute("SELECT * FROM users WHERE username=?", (ch * 8,))
+            cur.execute("SELECT * FROM users WHERE username=?", [ch * 8])
             if len(cur.fetchall()) > 0:
-                cur.execute("DELETE FROM users WHERE username=?", (ch * 8,))
+                cur.execute("DELETE FROM users WHERE username=?", [ch * 8])
 
         bot.reply_to(message, "Done")    
         dbdown(conn)
     else:
         bot.reply_to(message, "U r not mah masta")    
 
-#############################################################################
 
-@bot.message_handler(commands=['clear_db'])
+@bot.message_handler(commands=['clear_db'])		#clears database if needed to restart
 def clear_db(message):
     if verified(message.from_user.username):
         cur, conn = dbup()
@@ -146,7 +140,6 @@ def clear_db(message):
     else:
         bot.reply_to(message, "U r not mah masta") 
 
-#############################################################################
 
 # @bot.message_handler(commands=['create_db'])
 # def create_db(message):
@@ -156,6 +149,5 @@ def clear_db(message):
 #             )""")
 #     dbdown(conn)
 
-#############################################################################
 
 bot.polling()
